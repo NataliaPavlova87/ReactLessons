2aca58e5e384bf63821f8e0a819b95bacd864136
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-06-22 13:00:10 +0300

    Update ImageBox.jsx

--- src/components/ImageBox/ImageBox.jsx
+++ src/components/ImageBox/ImageBox.jsx
@@ -2,15 +2,21 @@ import './ImageBox.scss';
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { LikesComments } from '../LikesComments';
+import { Likes } from '../Likes';
+import { Comments } from '../Comments';
 
 export class ImageBox extends Component {
   render() {
     const { image, likes, comments } = this.props;
     return (
-      <div className="gallery-item" tabIndex="0">
-        <img src={image} className="gallery-image" alt="" />
-        <LikesComments likes={likes} comments={comments} />
+      <div className='gallery-item' tabIndex='0'>
+        <img src={image} className='gallery-image' alt='' />
+        <div className='gallery-item-info'>
+        <ul>
+          <Likes likes={likes} />
+          <Comments comments={comments} />
+        </ul>
+      </div>
       </div>
     );
   }
